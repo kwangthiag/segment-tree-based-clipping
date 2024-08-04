3d65579e9bc2c76ddf72b682169dd79977f5fe31
buddhi1 <buddhiashan8@gmail.com>
2024-08-04 03:57:03 -0500

    omp bahs updated

--- run-omp.sh
+++ run-omp.sh
@@ -1,10 +1,10 @@
 # ------------------------------
 # for ICPP
 OMP_NUM_THREADS=10 bin/./seg data/s.txt data/c.txt 
-# OMP_NUM_THREADS=10 bin/./seg data/ne_10m_ocean_0.txt data/continents_521.txt
-# OMP_NUM_THREADS=10 bin/./seg data/ne_10m_ocean_0.txt data/continents_1661.txt
-# OMP_NUM_THREADS=10 bin/./seg data/lakes_174690.txt data/parks_321571_-226-8_-3-5_lakes_174690_fx.txt
-# OMP_NUM_THREADS=10 bin/./seg data/lakes_174690.txt data/parks_34622_-230-6_-5-6_lakes_174690_fx.txt
-# OMP_NUM_THREADS=10 bin/./seg data/lakes_174690.txt data/parks_140315_-234-2_-6-8_lakes_174690_fx.txt
-# OMP_NUM_THREADS=10 bin/./seg data/lakes_174690.txt data/parks_169840_-230.2_-4.2_lakes_174690_fx.txt
+OMP_NUM_THREADS=10 bin/./seg data/ne_10m_ocean_0.txt data/continents_521.txt
+OMP_NUM_THREADS=10 bin/./seg data/ne_10m_ocean_0.txt data/continents_1661.txt
+OMP_NUM_THREADS=10 bin/./seg data/lakes_174690.txt data/parks_321571_-226-8_-3-5_lakes_174690_fx.txt
+OMP_NUM_THREADS=10 bin/./seg data/lakes_174690.txt data/parks_34622_-230-6_-5-6_lakes_174690_fx.txt
+OMP_NUM_THREADS=10 bin/./seg data/lakes_174690.txt data/parks_140315_-234-2_-6-8_lakes_174690_fx.txt
+OMP_NUM_THREADS=10 bin/./seg data/lakes_174690.txt data/parks_169840_-230.2_-4.2_lakes_174690_fx.txt
 # ------------------------------
